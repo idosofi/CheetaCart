f169da8c2ffc83c620a11da82439385b9f03821d
Ido Sofi <ido.sofi@gmail.com>
2017-11-29 10:01:37 +0200

    Applying numeric format on the CurrencyFormatter

--- app/src/main/java/com/sofiwares/cheetacart/data/formatter/CurrencyFormatter.kt
+++ app/src/main/java/com/sofiwares/cheetacart/data/formatter/CurrencyFormatter.kt
@@ -1,5 +1,8 @@
 package com.sofiwares.cheetacart.data.formatter
 
+import java.text.NumberFormat
+import java.util.*
+
 /**
  * Created by Ido Sofi on 11/27/2017.
  */
@@ -9,6 +12,6 @@ class CurrencyFormatter(private val value: Double) {
      * TODO: Handle different currencies
      */
     fun format(): String {
-        return "$$value"
+        return "$${NumberFormat.getNumberInstance(Locale.getDefault()).format(value)}"
     }
 }
